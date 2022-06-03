ef066c5c5ca950cbbbf28cc2257122e04a482534
sacrosanctry <winter27m@gmail.com>
2022-06-03 04:07:07 +0300

    correct weather display

--- weather_admin/views.py
+++ weather_admin/views.py
@@ -7,14 +7,10 @@ from django.urls import reverse
 from django.contrib import messages
 from django.shortcuts import redirect
 from weather_admin.models import Weather, City, Date
-from django.http import JsonResponse
 from datetime import datetime
 from django.utils.formats import get_format
 
-# from django.import log
-
 # Create your views here.
-
 
 
 def parse_date(date_str):
@@ -28,21 +24,7 @@ def parse_date(date_str):
     return None
 
 
-
 def home_view(request, *args, **kwargs):
-
-    class ListAsQuerySet(list):
-
-        def init(self, *args, model, **kwargs):
-            self.model = model
-            super().init(*args, **kwargs)
-
-        def filter(self, *args, **kwargs):
-            return self  # filter ignoring, but you can impl custom filter
-
-        def order_by(self, *args, **kwargs):
-            return self
-
 
     cities = City.objects.all() # query set
     dates = Date.objects.all()
@@ -55,9 +37,9 @@ def home_view(request, *args, **kwargs):
     # city_name = request.POST.get('city') # gets the first (0) element of list
     # # city_name = request.POST.get('city')
     # # city_name = City.objects.filter(city_name=city_name)
-    # print(city_name) # print в терминале
+    # print(city_name)
 
-
+    # date_name = request.POST.getlist('date')[0]
 
     if request.method == 'POST': # form>select in home.html # POST method is used to transfer data from client to server in HTTP
 
@@ -66,62 +48,84 @@ def home_view(request, *args, **kwargs):
         date_name = alldata.get("date", "0")
         print(city_name)
 
-
-        new_dates_list = []
-
-        dates_list = list(dates) # convert query set to list
-
-        parsed_date = parse_date(date_name)
-        query_date = Date.objects.get(date=parsed_date)
-
-        n = 0
-        for i in dates:
-            n += 1
-            if i == query_date:
-                new_dates_list = Date.objects.all()[n:(n+7)] # dates_list[dates_list.index(i):(dates_list.index(i)+6)]
-
-
-        # dates_queryset7 = ListAsQuerySet(new_dates_list, model=Date)
-
-
         if city_name != '0' and date_name != 'Date': # one of select options (city/date) is not chosen
+
+            dates_list = list(dates) # convert query set to list
+
+            parsed_date = parse_date(date_name)
+            query_date = Date.objects.get(date=parsed_date)
+
+            new_dates_list = []
+            n = 0
+            for i in dates:
+                n += 1
+                if i == query_date:
+                    new_dates_list = Date.objects.all()[n:(n+7)] # dates_list[dates_list.index(i):(dates_list.index(i)+6)]
 
             place = City.objects.get(name=city_name)
             city_id = place.id
             print(city_id)
 
-            try: 
-                weather_in_city = Weather.objects.get(id=2) #!!!!!!!WRONG ID!=2 problems with date (foreiggn field does not have date field)
-            except Weather.DoesNotExist:
-                weather_in_city = None
+            
+            # people = People.objects.order_by(name).filter(age__gt=65) # unevaluated
+            # people.query.set_limits(start, stop)  # still unevaluated
+            # for person in people:  # now its evaluated
+            #     person.do_the_thing()
+
+
+            filtered_allweather = all_weather.order_by('date').filter(city__name=city_name)
+
+            # ids = filtered_allweather.values_list('id', flat=True) # Cannot resolve keyword 'i' into field. Choices are: city, city_id, date, date_id, id, temperature, time, weather
+            chosen_date_id = filtered_allweather.get(date=query_date).id #! gets id from the first query set, not from filtered_allweather
+            # user = User.objects.get(username='Fokoa')
+            # user.id
+            # chosen_date_id = 2
+            done_weather = filtered_allweather.query.set_limits(low=chosen_date_id-1, high=chosen_date_id+6) #! low=chosen_date_id, high=chosen_date_id+7
+
+
+
+
+            # new_dates_list.filter('query_date')[:7] # error Cannot filter a query once a slice has been taken.
+
+            weather_in_city_list = []
+            # for j, k in zip(new_dates_list, range(len(new_dates_list))):
+            #     date_name2 = new_dates_list.get("date", "k")
+            #     parsed_date2 = parse_date(date_name2)
+            #     weather_in_city = Weather.objects.get(date__date=parsed_date2)
+            #     weather_in_city_list.append(weather_in_city)
+            
+            not_chosen_error = False    
 
         else:
+            not_chosen_error = True
             weather_in_city = None
+            weather_in_city_list = None
+            filtered_allweather = None
             city_id = None
-            place = None   
+            place = None
+            new_dates_list = None   
 
     else:
+        not_chosen_error = False
         weather_in_city = None
+        weather_in_city_list = None
+        filtered_allweather = None
         city_id = None
-        place = None   
+        place = None
+        new_dates_list = None   
 
-
-
-        # date_name = request.POST.getlist('date')[0] # gets the first (0) element of list
-        # print(date_name)
-
-        # date = Date.objects.get(date=date_name)
-
-    
-              
 
     context = {
-        'object': weather_in_city,
+        # 'object1': weather_in_city,
+        # 'weather_in_city_list': weather_in_city_list,
         'city': city_id,
         'cities': cities,
         'dates': dates,
-        'new_dates_list': new_dates_list,
         'place': place,
+        'new_dates_list': new_dates_list,
+        'not_chosen_error': not_chosen_error,
+        'filtered_allweather': filtered_allweather,
+        'done_weather': done_weather
     }
     return render(request, "home.html", context)
     # "home.html" - template name, {} - context ({} - empty dictionary)
